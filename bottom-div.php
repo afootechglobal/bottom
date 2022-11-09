5420936beb225f3ff35f83f75faa7c04d717a506
afootechglobal <afolabitaiwoabayomi112@gmail.com>
2022-11-09 04:30:41 +0100

    first update

---
+++ bottom-div.php
@@ -0,0 +1,19 @@
+<div class="live-chat-link-div" title="Chat With Us Now">
+<div class="live-chat-pix"><img src="images/cutomercare.jpg"/></div>
+<i class="fa fa-question"></i> Live Help
+</div>
+
+
+
+
+
+
+
+
+    <script src="js/aos.js"></script>
+    <script>
+      AOS.init({
+        easing: 'ease-in-out-sine'
+      });
+    </script>
+
