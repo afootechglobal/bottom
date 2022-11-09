ed31550c4c1621306bb24c9647bdb52a66a64e73
afootechglobal <afolabitaiwoabayomi112@gmail.com>
2022-11-09 04:41:27 +0100

    second updated

---
+++ why-cmi.php
@@ -0,0 +1,121 @@
+<?php require_once 'config/connection.php' ?>
+
+
+<?php 
+$url = str_replace('?', '&', $_SERVER['REQUEST_URI']);
+header($url);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php include 'links.php'?>
+<!-------------------------------------------------------------->
+<title>Why - <?php echo $thename?></title>
+<meta name="keywords" content />
+    <meta name="description" content="We offer medical degree programs at the best medical universities in Nigeria and Whole World. Compeer Medical assures a bright future in medical programs. "/>
+
+    <meta property="og:title" content="Why - <?php echo $thename?>" />
+    <meta property="og:image" content="<?php echo $website?>/image/seo_image/student-transfer-program.jpg"/>
+    <meta property="og:description" content="We offer medical degree programs at the best medical universities in Nigeria and Whole World. Compeer Medical assures a bright future in medical programs."/>
+
+    <meta name="twitter:title" content="Why- <?php echo $thename?>"/> 
+    <meta name="twitter:card" content="<?php echo $thename?>"/> 
+    <meta name="twitter:image"  content="<?php echo $website?>/image/seo_image/cmi-laboratory-image.jpg"/> 
+    <meta name="twitter:description" content="We offer medical degree programs at the best medical universities in Nigeria and Whole World. Compeer Medical assures a bright future in medical programs."/>
+</head>
+
+</head>
+<body>
+
+<?php include 'header.php'?>
+
+<div class="body-div">
+    <div class="background_div" class="aos-animate" data-aos="fade-down" data-aos-duration="1500">
+        <div class="over_lay"> 
+            <div class="text">
+                <h1>Why C.M.I</h1>
+                <p>Home / <span>Why CMI</span></p>
+            </div>
+        </div>
+    </div>
+
+  
+    
+    <div class="side_back_div">
+        <div class="div_in">
+            <div class="side">
+                    <div class="img_div" class="aos-animate" data-aos="zoom-in" data-aos-duration="1500">
+                         <img src="image/cmi-laboratory-image.jpg" alt="Why Compeer image"> 
+                    </div>
+                    <div class="img_div link_div" data-aos="fade-up" data-aos-duration="1000">
+                        <div class="header_link"><i class="fa fa-link"></i>  <h2>Hot Links</h2> </div>
+                                <div class="li">
+                                    <li class="active" title="About CMI">About C.M.I</li>
+                                    <li title="Why CMI">Why C.M.I</li>
+                                    <li title="CMI Services">C.M.I Services</li>
+                                    <li title="Accreditation & Approval">Accreditation & Approval</li>
+                                    <li title="Clinical">Affiliate Universities</li>
+                                </div>
+                        </div>
+                    </div>
+
+            <div class="side right_side">
+            <h2>WHY US</h2>
+            <hr style="border:2px #ccc solid; width:40%;margin-left:0px">
+                    <p>
+                    As an Institute, we are uniquely capable of the kind of creative thinking and program development that traditional and alternative universities might not have. Flexible, 
+                    innovative and always moving forward, our top thinkers are constantly working to reshape programs and services to the needs of current and future students. One of our mottos is: <strong>Not Just Doctors</strong>. Our world recognized and accredited programs are built to instill a spirit of Social Entrepreneurship and World Citizenship as well as create world-class physicians. Because of this, our graduates not only succeed within their professions, but are also spearheading a return to the fundamental and humane practice of the Science and Art of Medicine.</p>
+                  
+                    <span class="subject"><i class="fa fa-check"></i> <strong>We hand-pick our students.</strong> </span> By recruiting Brilliant, Exceptional, Boldly Original men and women who exhibit the capacity and drive to Serve a World in Need,we know that should you accept our offer, you too will come to change the world. <br>
+                    <span class="subject"><i class="fa fa-check"></i> <strong>Partnership is our greatest strength.</strong> By striking unique partnerships with universities from among Europe’s most storied Academic Institutions, we created beyond Best-in-Class academic programs and World-Class support services tailored specifically for our candidates, students and graduates. <br>
+
+                    <span class="subject"><i class="fa fa-check"></i> <strong>Affordable opportunity is key</strong> We increased access and opportunity for those students who are ready to change the world. And though you will receive more at C.M.I. than most US or Foreign Medical Schools offer, you will pay much less than you will at any of these schools. <br>
+                    <span class="subject"><i class="fa fa-check"></i> <strong>Global Recognition is our standard</strong> Our partner universities are recognized by various Licensing Authorities within the USA, Canada, Europe and many other countries across the globe and have been recognized by major State Medical Boards including California & New York (2 states with the most stringent rules for accreditation and the most residency positions in the USA). <br>
+                    <span class="subject"><i class="fa fa-check"></i> <strong>Environment must be considered.</strong>Purposefully housed on beautiful, safe campuses within a train ride from destinations like Paris and Amsterdam, our attention to detail offers students very special learning opportunities – because the best deserve the best. <br>
+                    <span class="subject"><i class="fa fa-check"></i> <strong>We consider above-and-beyond a starting point.</strong> C.M.I. delivers unmatched learning and research opportunities to help students enhance potential choice of residency. Clinical Rotations/Clerkships are offered at ACGME approved and accredited US and European hospitals.
+                    <span class="subject"><i class="fa fa-check"></i> <strong>Opportunity has costs but shouldn’t be costly.</strong> Although our costs are much lower than most, each Partner University is certified by the U.S. Department of Education to participate in the William D. Ford Federal Direct Loan Programs. <br>
+                    <span class="subject"><i class="fa fa-check"></i> <strong>Our Commitment is for a Lifetime: </strong> Our Life Support Services are second to none. Our trained C.M.I. staff, on campuses and in the US, are here to assist you with academic, cultural, and social adjustments. Our graduate services help with lifelong transitions and continuing education. It is our aim to provide each and every candidate, student and graduate with unparalleled support — because that is what you deserve. <br>
+                    <span class="subject"><i class="fa fa-check"></i> <strong>Bigger is not always better.</strong> We want you to learn as much as you can so we do not pack our classes. This allows for intimate, personalized class sizes ensuring every student has the opportunity to obtain a strong medical education. All classes are taught in English. <br>
+                    <span class="subject"><i class="fa fa-check"></i> <strong>Success is a family affair</strong> We want your family and friends to celebrate your journey with us that is why matriculation and graduation ceremonies are held in the US to ensure attendance by those who supported you the most. <br> <br>
+
+                    Department of Education certification and a determination of program eligibility are required in order for students to apply for such loans to help finance the cost of attending the Universities. The Universities and Compeer Medical Institute (CMI) do not guarantee the status or continuation of the Universities’ certification or eligibility to participate in federal student financial aid programs or the availability of financial aid to any entering or continuing students during their entire educational period.
+            <br> <br>
+                <div class="btn" title="Contact Us">CONTACT US NOW <i class="fa fa-arrow-right"></i></div>
+            </div>
+
+    </div>
+
+    </div>
+
+
+</div>
+
+
+
+<?php include 'footer.php' ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+<script>
+    AOS.init({
+    easing: 'ease-in-out-sine'
+    });
+</script>
